3ed9ad6c19cc5e92da5d6a9f41615c0daaddb527
Vitalis011 <vitalisugorji1@gmail.com>
2025-02-02 22:22:36 +0200

    adding variables solution

---
+++ week1/variables/variables01.js
@@ -0,0 +1,11 @@
+/* 
+Create a variable named count and set its value to 0.
+On the next line, add 1 to count.
+Use console.log() to print the value of count. 
+*/
+
+let count = 0; //count += 1;
+
+count = count + 1; //count++;
+
+console.log("count: ", count);
