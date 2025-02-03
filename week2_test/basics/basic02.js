afa1d9b7dae0a73f1b806bd65edbb4401a66bae9
Vitalis011 <vitalisugorji1@gmail.com>
2025-02-03 22:51:22 +0200

    solutions to basic task for week 3

---
+++ week2_test/basics/basic02.js
@@ -0,0 +1,15 @@
+/* Create a function named isEven that:
+Takes one parameter, num.
+Returns true if num is even, otherwise returns false.
+Call the function with the arguments 4 and 7.
+Print the results using console.log().
+
+Bonus: Modify the function to use implicit return. 
+*/
+
+const isEven = (num) => {
+  return num % 2 === 0;
+};
+
+console.log(isEven(4)); //true
+console.log(isEven(7)); //false
