d853ae70e73132bf4167862f0ee027178dac882f
Vitalis011 <vitalisugorji1@gmail.com>
2025-02-03 23:43:52 +0200

    save untracked changes before rebase

---
+++ week04/mains.js
@@ -0,0 +1,13 @@
+const circle = document.getElementById("alert");
+console.log(circle.textContent);
+const paragragh = document.getElementsByTagName("p");
+console.log(paragragh.innerHTML);
+
+function myFunction() {
+  alert("some cool function");
+}
+myFunction();
+
+function myFunction2() {
+  alert("i told you not to click!");
+}
